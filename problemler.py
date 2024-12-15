26db90a16d9192d7bd4c3fc064bc562a8ce04b32
Baran Koç <19240000971@ogrenci.ege.edu.tr>
2024-12-15 10:26:03 +0300

    Update problemler.py

--- problemler.py
+++ problemler.py
@@ -1,14 +1,4 @@
-# sayi = int(input("Bir sayı giriniz: "))
-# for i in range(2,sayi+1):
-#     if sayi%i==0:
-#         for j in range(2,i):
-#             if i%j==0:
-#                 break
-#         else:
-#             print(i,"Sayısı",sayi,"Sayısının Asal Çarpanıdır.")
-
-
-# sayi=int(input("Bir sınır tuşlayınız: "))
+# sayi=int(input("Bir sınır değeri tuşlayınız: "))
 # for i in range(2,sayi+1):
 #     for j in range(2,i):
 #         if i%j==0:
